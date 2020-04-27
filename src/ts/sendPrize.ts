656f7fd12b62f4cbf775f39148f366bc5ebd2523
Patrick McCorry <stonecoldpat@gmail.com>
2020-04-27 18:59:22 +0100

    Script to let the winner claim their prize

---
+++ src/ts/sendPrize.ts
@@ -0,0 +1,197 @@
+//      ___      .__   __. ____    ____      _______. _______ .__   __.  _______   _______ .______
+//     /   \     |  \ |  | \   \  /   /     /       ||   ____||  \ |  | |       \ |   ____||   _  \
+//    /  ^  \    |   \|  |  \   \/   /     |   (----`|  |__   |   \|  | |  .--.  ||  |__   |  |_)  |
+//   /  /_\  \   |  . `  |   \_    _/       \   \    |   __|  |  . `  | |  |  |  ||   __|  |      /
+//  /  _____  \  |  |\   |     |  |  __ .----)   |   |  |____ |  |\   | |  '--'  ||  |____ |  |\  \----.
+// /__/     \__\ |__| \__|     |__| (__)|_______/    |_______||__| \__| |_______/ |_______|| _| `._____|
+//                          ______   ______   .___  ___. .______    __
+//                         /      | /  __  \  |   \/   | |   _  \  |  |
+//                        |  ,----'|  |  |  | |  \  /  | |  |_)  | |  |
+//                        |  |     |  |  |  | |  |\/|  | |   ___/  |  |
+//                        |  `----.|  `--'  | |  |  |  | |  |      |__|
+//                         \______| \______/  |__|  |__| | _|      (__)
+
+import { ethers, Wallet } from "ethers";
+import { Provider } from "ethers/providers";
+
+import {
+  sendToAnySender,
+  watchRelayTx,
+  onchainDepositFor,
+  getAnySenderBalance,
+} from "./utils";
+import { CyberDiceFactory } from "../typedContracts/CyberDiceFactory";
+import {
+  INFURA_PROJECT_ID,
+  USER_MNEMONIC,
+  NETWORK_NAME,
+  DEPOSIT_CONFIRMATIONS,
+  CYBERDICE_CONTRACT_ADDRESS,
+  PRIVATE_KEY,
+} from "./config";
+import { parseEther, formatEther } from "ethers/utils";
+
+/**
+ * Set up the provider and wallet
+ */
+async function setup() {
+  const infuraProvider = new ethers.providers.InfuraProvider(
+    NETWORK_NAME,
+    INFURA_PROJECT_ID
+  );
+
+  if (USER_MNEMONIC.length > 0 && PRIVATE_KEY.length > 0) {
+    console.log(
+      "Both 12-word seed and private key is filled in. Please only use one option!"
+    );
+    process.exit(0); // thanks to Sascha for pointing this out.
+  }
+
+  if (USER_MNEMONIC.length > 0) {
+    const userMnemonicWallet = ethers.Wallet.fromMnemonic(USER_MNEMONIC);
+    const user = userMnemonicWallet.connect(infuraProvider);
+
+    return {
+      user,
+      provider: infuraProvider,
+    };
+  }
+
+  if (PRIVATE_KEY.length > 0) {
+    const userMnemonicWallet = new Wallet(PRIVATE_KEY);
+    const user = userMnemonicWallet.connect(infuraProvider);
+
+    return {
+      user,
+      provider: infuraProvider,
+    };
+  }
+
+  console.log(
+    "No user credentials in config.ts. \nRun 'npm run generateSeed' to compute a new wallet for the competition."
+  );
+  process.exit(0);
+}
+
+/**
+ * Sends tickets to the CyberDice contract via any.sender
+ * @param message Message to submit
+ * @param user User's wallet
+ * @param provider InfuraProvider
+ * @param cyberDiceCon CyberDice Contract
+ */
+async function sendPrize(
+  user: Wallet,
+  provider: Provider,
+  cyberDiceCon: ethers.Contract
+) {
+  /**
+   * We need to compute the calldata which is the function name and its arguments.
+   * As you can see below, it is super-straight forward to encode the calldata
+   * using etherjs.
+   */
+  const callData = cyberDiceCon.interface.functions.sendPrize.encode([]);
+
+  const { relayTx, anySenderReceipt } = await sendToAnySender(
+    cyberDiceCon,
+    callData,
+    user,
+    provider
+  );
+
+  console.log(anySenderReceipt);
+
+  /**
+   * We simply watch for an event in the any.sender relay contract to verify
+   * when the relay transaction gets mined.
+   */
+  await watchRelayTx(relayTx, user, provider);
+
+  const balance = await provider.getBalance(user.address);
+  console.log(
+    "New balance of " + user.address + " is  " + formatEther(balance)
+  );
+}
+
+/**
+ * Sends prize to the winner
+ */
+(async () => {
+  // ricmoo this is ur fault
+  var log = console.log;
+  console.log = (messages: string) => {
+    if (messages === "WARNING: unsupported ABI type - receive") return;
+    else log(messages);
+  };
+
+  if (INFURA_PROJECT_ID.length === 0) {
+    console.log("Please open config.ts and fill in INFURA_PROJECT_ID");
+    return;
+  }
+  // Set up wallets & provider
+  const { user, provider } = await setup();
+
+  // Get CyberDice contract
+  const cyberDiceCon = new CyberDiceFactory(user).attach(
+    CYBERDICE_CONTRACT_ADDRESS
+  );
+
+  console.log(
+    "*** You are running the any.sender competition script on " +
+      NETWORK_NAME +
+      " ***"
+  );
+
+  console.log("Your wallet address: " + user.address);
+
+  // Sanity check minimum balance.
+  let anySenderBalance = await getAnySenderBalance(user);
+  const ethereumBalance = await provider.getBalance(user.address);
+
+  // Checks on-chain balance and any.sender balance
+  if (anySenderBalance.lt(parseEther("0.009"))) {
+    if (ethereumBalance.gte(parseEther("0.01"))) {
+      // We need to wait for on-chain confirmations before any.sender
+      // accepts the deposit.
+      console.log(
+        "Sending on-chain deposit - wait ~" +
+          DEPOSIT_CONFIRMATIONS +
+          " confirmations. (Click link to check confirmations so far)"
+      );
+      await onchainDepositFor(parseEther("0.009"), user);
+    } else {
+      console.log("Your ethereum wallet lacks the funds to top up any.sender.");
+      console.log("Your ethereum balance is " + formatEther(anySenderBalance));
+      console.log("Please top up to at least 0.01 eth");
+      return;
+    }
+  }
+
+  // What is the user's any.sender balance?
+  anySenderBalance = await getAnySenderBalance(user);
+  console.log("Balance on any.sender: " + formatEther(anySenderBalance));
+
+  const winner = await cyberDiceCon.winner();
+  console.log("Winner: " + winner);
+
+  if (winner !== user.address) {
+    console.log(
+      "Sorry, you are not the winner :( Thank you for participating. We hope it was a lot of fun!"
+    );
+    process.exit(0);
+  }
+
+  const balance = await provider.getBalance(winner);
+
+  console.log(
+    "Sending prize to " +
+      user.address +
+      " and its current balance is " +
+      formatEther(balance)
+  );
+  // Send ticket to any.sender
+  await sendPrize(user, provider, cyberDiceCon);
+})().catch((e) => {
+  console.log(e);
+  // Deal with the fact the chain failed
+});
